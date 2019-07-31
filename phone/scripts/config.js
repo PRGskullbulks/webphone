39bd0c14d6b330bbe899a4b33314a116cc90acfd
root <root@phonehub-webrtc.us-east1-d.c.precise-valor-239214.internal>
2019-07-31 03:17:41 +0000

    initial commit

---
+++ phone/scripts/config.js
@@ -0,0 +1,12 @@
+var user = {
+    //  User Name
+    "User" : "101",
+    //  Password
+    "Pass" : "S0p0rt3s0s.",
+    //  Auth Realm
+    "Realm"   : "phonehub",
+    // Display Name
+    "Display" : "Sip Softphone",
+    // WebSocket URL
+    "WSServer"  : "wss://sip.phonehub.com.co:24443/ws"
+};
